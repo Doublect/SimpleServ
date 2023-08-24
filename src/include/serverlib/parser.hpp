88e3241561dca34517a9543586d5956564aeaa3e
Hunor Liszka <hunor.liszka@protonmail.com>
2023-08-24 16:48:22 +0200

    Fix stylng

--- src/include/serverlib/parser.hpp
+++ src/include/serverlib/parser.hpp
@@ -9,14 +9,14 @@
 
 class parser_error : public std::exception {
 public:
-  parser_error() {}
-  ~parser_error() {}
+	parser_error() {}
+	~parser_error() {}
 };
 
 std::expected<HTTPRequest, parser_error> parse_http_request(std::string str);
 std::expected<
-    std::tuple<std::map<std::string_view, std::string_view>, std::string_view>,
-    parser_error>
+		std::tuple<std::map<std::string_view, std::string_view>, std::string_view>,
+		parser_error>
 parse_message_headers(std::string_view str);
 
 #endif
